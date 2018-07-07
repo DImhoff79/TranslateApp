d6f305c17f314047077dc327072a27c809608bf6
210039955 <david.imhoff@ge.com>
2018-07-06 21:42:49 -0400

    stage this

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var createError = require('http-errors');
+var helmet = require('helmet');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
+var jsonParser = bodyParser.json();
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+////send form to be parsed
+router.post('/sendit',jsonParser,function(req,res){
+  var target = req.body.target;
+  var text = req.body.text;
+  
+
+//calls the translate.translate function and returns the results array
+
+ //send the results of the translation along with the initial message back to the index page
+ res.render('index', {result: "results here", keep:"original text"});
+ //console.log(translation);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  
+
 });
 
 module.exports = router;
