5bdfa66fec73ec311e444a982118136bc432dec9
210039955 <david.imhoff@ge.com>
2018-07-06 22:06:47 -0400

    Added AWS SDK

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var router = express.Router();
 var jsonParser = bodyParser.json();
 var unenParser = bodyParser.urlencoded();
-
+var translate = new AWS.Translate();
 
 
 
@@ -18,6 +18,17 @@ router.post('/sendit',jsonParser,function(req,res){
   var target = req.body.target;
   var text = req.body.text;
   
+  var params = {
+    SourceLanguageCode: 'auto', /* required */
+    TargetLanguageCode: req.body.target, /* required */
+    Text: req.body.text /* required */
+  };
+
+  translate.translateText(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+
 // Instantiates a client
 const translate = new Translate({
   projectId: projectId,
