9ecfca4284ffc1211689b58452dbc31c2b958ce1
210039955 <david.imhoff@ge.com>
2018-07-06 21:53:09 -0400

    fix styles

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
 var jsonParser = bodyParser.json();
+var unenParser = bodyParser.urlencoded();
 
 
 
@@ -17,17 +18,25 @@ router.post('/sendit',jsonParser,function(req,res){
   var target = req.body.target;
   var text = req.body.text;
   
+// Instantiates a client
+const translate = new Translate({
+  projectId: projectId,
+});
 
 //calls the translate.translate function and returns the results array
-
+translate
+  .translate(text, target)
+  .then(results => {
+ const translation = results[0];
+ 
  //send the results of the translation along with the initial message back to the index page
- res.render('index', {result: "results here", keep:"original text"});
+ res.render('index', {result: results[0], keep:req.body.text});
  //console.log(translation);
   })
   .catch(err => {
     console.error('ERROR:', err);
-  
+  });
 
-});
+})
 
 module.exports = router;
